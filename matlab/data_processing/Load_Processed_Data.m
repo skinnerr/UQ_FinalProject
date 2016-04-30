e237d0d0957352b00209db7ee7f150adedbb8898
Ryan Skinner <anomaly1452@gmail.com>
2016-04-30 13:13:18 -0600

    Move computation of averaged x, and cp statistics to Load_Processed_Data.m

--- matlab/data_processing/Load_Processed_Data.m
+++ matlab/data_processing/Load_Processed_Data.m
@@ -1,4 +1,5 @@
-function [ NACA_data ] = Load_Processed_Data( identifier_string, nfiles, alpha_threshold )
+function [ NACA_data, NACA_stats ] = Load_Processed_Data( identifier_string, nfiles, ...
+                                                          alpha_threshold )
 
     fprintf('Reading %i files from %s\n',nfiles,identifier_string);
 
@@ -27,6 +28,37 @@ function [ NACA_data ] = Load_Processed_Data( identifier_string, nfiles, alpha_t
     else
         fprintf('After removing alpha > %.2f, %i files remain\n',alpha_threshold,nfiles);
     end
+    
+    x_avg  = zeros(length(NACA_data(1).xnorm),1);
+    cp_max = NACA_data(1).cp;
+    cp_min = cp_max;
+    cp_avg = zeros(length(cp_max),1);
+    cp_var = zeros(length(NACA_data(1).xnorm),1);
+    for i = 1:nfiles
+        x_avg = x_avg + NACA_data(i).xnorm / nfiles;
+        cp_avg = cp_avg + NACA_data(i).cp / nfiles;
+        cp_var = cp_var + (NACA_data(i).cp).^2 / nfiles;
+        ind = NACA_data(i).cp > cp_max;
+        cp_max(ind) = NACA_data(i).cp(ind);
+        ind = NACA_data(i).cp < cp_min;
+        cp_min(ind) = NACA_data(i).cp(ind);
+    end
+    cp_var = cp_var - cp_avg.^2;
+    cp_std = sqrt(cp_var);
+    
+    % Create x_avg_twosurf coordinate, which has x<0 on upper surface, x>0 on lower
+    % surface, and x=0 on the trailing edge.
+    trailing_x = find(x_avg == max(x_avg),1);
+    x_avg_twosurf = [x_avg(1:trailing_x); max(x_avg)*2 - x_avg(trailing_x+1:end)];
+    x_avg_twosurf = x_avg_twosurf - max(x_avg);
+    
+    NACA_stats.x_avg = x_avg;
+    NACA_stats.cp_max = cp_max;
+    NACA_stats.cp_min = cp_min;
+    NACA_stats.cp_avg = cp_avg;
+    NACA_stats.cp_var = cp_var;
+    NACA_stats.cp_std = cp_std;
+    NACA_stats.x_avg_twosurf = x_avg_twosurf;
 
 end
 
--- matlab/data_processing/Plot_Cp.m
+++ matlab/data_processing/Plot_Cp.m
@@ -4,50 +4,28 @@ function [] = Plot_Cp()
 
     nfiles = 100;
     alpha_threshold = 0;
-    NACA_data = Load_Processed_Data('LF-1000', nfiles, alpha_threshold);
+    [Ndat, Nstat] = Load_Processed_Data('LF-1000', nfiles, alpha_threshold);
 
     %%%
     % Plot stuff!
     %%%
     
-    x_avg  = zeros(length(NACA_data(1).xnorm),1);
-    cp_max = NACA_data(1).cp;
-    cp_min = cp_max;
-    cp_avg = zeros(length(cp_max),1);
-    cp_var = zeros(length(NACA_data(1).xnorm),1);
-    for i = 1:nfiles
-        x_avg = x_avg + NACA_data(i).xnorm / nfiles;
-        cp_avg = cp_avg + NACA_data(i).cp / nfiles;
-        cp_var = cp_var + (NACA_data(i).cp).^2 / nfiles;
-        ind = NACA_data(i).cp > cp_max;
-        cp_max(ind) = NACA_data(i).cp(ind);
-        ind = NACA_data(i).cp < cp_min;
-        cp_min(ind) = NACA_data(i).cp(ind);
-    end
-    cp_var = cp_var - cp_avg.^2;
-    cp_std = sqrt(cp_var);
-    
-    % Create x_avg_twosurf coordinate, which has x<0 on upper surface, x>0 on lower
-    % surface, and x=0 on the trailing edge.
-    trailing_x = find(x_avg == max(x_avg),1);
-    x_avg_twosurf = [x_avg(1:trailing_x); max(x_avg)*2 - x_avg(trailing_x+1:end)];
-    x_avg_twosurf = x_avg_twosurf - max(x_avg);
-    
     figure();
     hold on;
-    X = [x_avg_twosurf; flip(x_avg_twosurf)];
-    Y2 = [cp_avg+3*cp_std; flip(cp_avg-3*cp_std)];
+    X = [Nstat.x_avg_twosurf; flip(Nstat.x_avg_twosurf)];
+    Y2 =     [Nstat.cp_avg+3*Nstat.cp_std; ...
+         flip(Nstat.cp_avg-3*Nstat.cp_std)];
     fill(X,Y2,[0.95, 1.0, 0.95]);
-    Y1 = [cp_max; flip(cp_min)];
+    Y1 = [Nstat.cp_max; flip(Nstat.cp_min)];
     fill(X,Y1,[0.95, 0.95, 1.0]);
     for i = 1:nfiles
-        hdots = plot(x_avg_twosurf, NACA_data(i).cp, 'k.', 'MarkerSize', 3);
+        hdots = plot(Nstat.x_avg_twosurf, Ndat(i).cp, 'k.', 'MarkerSize', 3);
     end
-    hrange = plot(x_avg_twosurf, cp_max, 'b-');
-             plot(x_avg_twosurf, cp_min, 'b-');
-    h3std  = plot(x_avg_twosurf, cp_avg+3*cp_std, 'g-');
-             plot(x_avg_twosurf, cp_avg-3*cp_std, 'g-');
-    hmean  = plot(x_avg_twosurf, cp_avg, 'r-');
+    hrange = plot(Nstat.x_avg_twosurf, Nstat.cp_max, 'b-');
+             plot(Nstat.x_avg_twosurf, Nstat.cp_min, 'b-');
+    h3std  = plot(Nstat.x_avg_twosurf, Nstat.cp_avg+3*Nstat.cp_std, 'g-');
+             plot(Nstat.x_avg_twosurf, Nstat.cp_avg-3*Nstat.cp_std, 'g-');
+    hmean  = plot(Nstat.x_avg_twosurf, Nstat.cp_avg, 'r-');
     ylim([-3,1.5]);
     xlabel('Streamwise Location CW from Trailing Edge [1 / Streamwise Chord]');
     ylabel('Coefficient of Pressure');
